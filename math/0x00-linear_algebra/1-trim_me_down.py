cccee97b0a5eed2bfe8048ba1d44119e2be8e552
Camilo Barreiro <camilobarreiro96@gmail.com>
2023-05-29 11:06:13 -0500

    update

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-middle_start = len(matrix[0]) // 2 - 1
-middle_end = middle_start + 2
-the_middle = [[row[i] for i in range(middle_start, middle_end)] for row in matrix]
+# Extract the middle columns
+the_middle = [row[2:4] for row in matrix]
+# Print the result
 print("The middle columns of the matrix are: {}".format(the_middle))
