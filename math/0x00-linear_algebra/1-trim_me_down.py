ad9114c00b7ef16d88b722042260330089ca5577
Camilo Barreiro <camilobarreiro96@gmail.com>
2023-05-29 10:52:33 -0500

    update

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
-matrix = np.array([[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]])
-middle_start = matrix.shape[1]// 2 - 1
+from operator import itemgetter
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+middle_start = len(matrix[0]) // 2 - 1
 middle_end = middle_start + 2
-the_middle = matrix[:, middle_start:middle_end]
+the_middle = list(map(itemgetter(slice(middle_start, middle_end)), matrix))
 print("The middle columns of the matrix are: {}".format(the_middle))
