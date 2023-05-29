ba3caae9ee432bec84c327913052284c9167d263
Camilo Barreiro <camilobarreiro96@gmail.com>
2023-05-29 10:54:24 -0500

    update

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from operator import itemgetter
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 middle_start = len(matrix[0]) // 2 - 1
 middle_end = middle_start + 2
-the_middle = list(map(itemgetter(slice(middle_start, middle_end)), matrix))
+the_middle = [[row[i] for i in range(middle_start, middle_end)] for row in matrix]
 print("The middle columns of the matrix are: {}".format(the_middle))
